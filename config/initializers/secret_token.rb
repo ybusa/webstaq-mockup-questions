06c8962417645906aa0f3c8965d0d182eb7198c9
Andrew <adharris@gmail.com>
2011-03-02 12:27:48 -0500

    Created Ruby Project
    Created the default ruby project.  Ran with -JT, in order to omit the
    Prototype js files (will be using jquery) and the Test::Unit files (will
    be using rspec).

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WebstaqMockupQuestions::Application.config.secret_token = 'ff020c787e96c6b45514b6df26c7c31569e1a087e30633ecb76d07a15bf0c70154f5c5ab7b2e1ee2468d1aa1b34a7eb75636e4c25d219c3a3c04c97060ae17af'
