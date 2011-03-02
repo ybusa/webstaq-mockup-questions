e1ed1ac48b8783135d219b5924fb8f070a8970c9
Andrew <adharris@gmail.com>
2011-03-02 13:20:17 -0500

    replaced default generators to generate haml
    Used http://github.com/mbleigh/haml-rails to replace ERB scaffolding
    with HAML.
    
    The plugin is checked out as a git submodule.  I have very little idea
    what that means. :-)

--- config/application.rb
+++ config/application.rb
@@ -38,5 +38,11 @@ module WebstaqMockupQuestions
 
     # Configure sensitive parameters which will be filtered from the log file.
     config.filter_parameters += [:password]
+
+    # Tell generators to generate haml files instead of erb
+    config.generators do |g|
+      g.template_engine :haml
+    end
   end
 end
+
